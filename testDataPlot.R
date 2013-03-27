68193c79cd2bced379267e1b3a392b1edd773d9c
John Smith <john@smith.com>
2013-03-27 13:14:42 +0000

    Add boxplot y axis scaling to include limit lines

--- testDataPlot.R
+++ testDataPlot.R
@@ -23,14 +23,14 @@ multi.sapply = function(...) {
 #fixme: Autocorrelation spectrum
 
 boxplotMain="Dispense accuracy: Volume [uL]"
-testGrepStr = "^T11.4"
+testGrepStr = "^T11.3"
 setTestNamesOveride = F
-testNamesOveride = c("T11.4B","T11.4C")
+testNamesOveride = c("T11.4D","T11.4I")
 summaryStatsRound = 1
-setAbline = T
-ablineMin = -100
-ablineMax = 100
-ablineNom = 0
+setAbline = F
+ablineMin = 90
+ablineMax = 110
+ablineNom = 100
 
 marDefault = c(5.1,4.1,4.1,2.1)
 layoutHeights = c(2,1)
@@ -57,13 +57,21 @@ if (setTestNamesOveride) {
 Ntests = length(testNames)
 testData = testDatabase[testNames]
 
+if (setAbline) {
+  ylimMin = min(testData,ablineMin,na.rm=T)
+  ylimMax = max(testData,ablineMax,na.rm=T)  
+} else {
+  ylimMin = min(testData,na.rm=T)
+  ylimMax = max(testData,na.rm=T)
+}
+
 layoutMatrix = matrix(1,2,Ntests+1)
 layoutMatrix[2,] = layoutMatrix[2,] + 1:(Ntests+1)
 layout(layoutMatrix,heights=layoutHeights)
 
-boxplot(testData,main=boxplotMain,notch=T,varwidth=T,col="lightblue",outpch=1,outcex=1.5)
+boxplot(testData,main=boxplotMain,notch=T,varwidth=T,col="lightblue",outpch=1,outcex=1.5,ylim=c(ylimMin,ylimMax))
 grid(nx=NA,ny=NULL)
-stripchart(testData,vertical=T,method="jitter",jitter=0.25,pch=21,cex=1.25,col="maroon",bg="bisque",add=T) 
+stripchart(testData,vertical=T,method="jitter",jitter=0.25,pch=21,cex=1.25,col="maroon",bg="bisque",add=T,ylim=c(ylimMin,ylimMax)) 
 if (setAbline) {
   abline(h=c(ablineMin,ablineMax),col="red",lwd=2.0)
   abline(h=ablineNom,col="green",lwd=2.0)
